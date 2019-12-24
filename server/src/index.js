bd846fdb3b498343a9db9646fb4240a8b27b46e0
Marc Brathwaite <marcbrathwaite@gmail.com>
2019-12-24 06:22:15 -0500

    change index.html file read to asynchronouse version

--- server/src/index.js
+++ server/src/index.js
@@ -13,27 +13,30 @@ const app = express()
 app.use('/static', express.static('./dist'))
 
 app.get('*', function(req, res) {
-  const contentFromHTMLFile = fs.readFileSync('./dist/index.html', 'utf-8')
-
-  const { path } = req
-
-  const promises = getData(routes, path)
-
-  Promise.all(promises).then(responses => {
-    // define contetxt object and pass it into renderer function
-
-    const context = { data: {} }
-    responses.forEach(r => {
-      if (r) {
-        Object.assign(context.data, r)
-      }
-    })
-
-    const content = renderer(path, contentFromHTMLFile, context)
-    if (context.notFound) {
-      res.status(404) // if user chooses incorrect route, send 404
+  fs.readFile('./dist/index.html', 'utf-8', (err, contentFromHTMLFile) => {
+    if (err) {
+      return res.status(500)
     }
-    res.send(content)
+    const { path } = req
+
+    const promises = getData(routes, path)
+
+    Promise.all(promises).then(responses => {
+      // define contetxt object and pass it into renderer function
+
+      const context = { data: {} }
+      responses.forEach(r => {
+        if (r) {
+          Object.assign(context.data, r)
+        }
+      })
+
+      const content = renderer(path, contentFromHTMLFile, context)
+      if (context.notFound) {
+        res.status(404) // if user chooses incorrect route, send 404
+      }
+      res.send(content)
+    })
   })
 })
 
